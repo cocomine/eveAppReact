46bc3cac19be3c800f505ea456743cb6328a760a
cocomine <roguish.6888467468@gmail.com>
2023-08-28 05:42:09 +0800

    code push

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @format
  */
 
-import {AppRegistry, Platform} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import {decode, encode} from 'base-64';
-import PushNotification from 'react-native-push-notification';
+import { AppRegistry, Platform } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { decode, encode } from "base-64";
+import PushNotification from "react-native-push-notification";
 
 if (!global.btoa) {
     global.btoa = encode;
