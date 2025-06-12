8f13fa3612e3480044d5a3b04af66fbaa600013c
cocomine <cocomine@cocomine.cc>
2025-06-12 17:19:28 +0800

    upgrade React Native 0.77.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
     root: true,
     extends: '@react-native',
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                trailingComma: 'all',
+                printWidth: 100,
+                tabWidth: 4,
+                semi: true,
+                endOfLine: 'auto',
+            },
+        ],
+        'react-native/no-inline-styles': 'off',
+        'react-native/no-color-literals': 'off',
+        'react-native/no-raw-text': 'off',
+        'react-native/no-unused-styles': 'warn',
+    },
 };
