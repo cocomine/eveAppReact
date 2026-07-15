124ee918ec0e588321e007c436a91befca3e8145
cocomine <cocomine@cocomine.cc>
2026-07-16 00:04:30 +0800

    refactor: improve code formatting and update dependencies in App.js, Export.js, and jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     preset: 'react-native',
+    setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+        '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve('react-native/jest/assetFileTransformer'),
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|react-native-.*|@react-native|@react-native-community|@react-navigation)/)',
+    ],
 };
